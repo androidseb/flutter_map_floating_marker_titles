78ff4355a7baffa2a041792211202611d7742f5d
androidseb <theandroidseb@gmail.com>
2021-04-03 11:25:10 -0400

    core: fixed some issues with state handling

--- flutter_floating_map_marker_titles_core/lib/view/abstract_map_view_wrapper.dart
+++ flutter_floating_map_marker_titles_core/lib/view/abstract_map_view_wrapper.dart
@@ -4,7 +4,7 @@ import 'package:flutter_floating_map_marker_titles_core/controller/map_view_inte
 import 'package:flutter_floating_map_marker_titles_core/model/floating_marker_title_info.dart';
 import 'package:flutter_floating_map_marker_titles_core/view/floating_marker_titles_overlay.dart';
 
-abstract class AbstractMapViewWrapper<T extends AbstractMapViewInterface> extends StatefulWidget {
+abstract class AbstractMapViewWrapper<T extends AbstractMapViewInterface> extends StatelessWidget {
   final T _mapViewInterface;
   final List<FloatingMarkerTitleInfo> _floatingTitles;
   final FMTOOptions _fmtoOptions;
@@ -16,32 +16,13 @@ abstract class AbstractMapViewWrapper<T extends AbstractMapViewInterface> extend
     Key key,
   }) : super(key: key);
 
-  @override
-  _AbstractMapViewWrapperState createState() => _AbstractMapViewWrapperState(
-        this._mapViewInterface,
-        this._floatingTitles,
-        this._fmtoOptions,
-      );
-
   Widget buildMapView(final BuildContext context, final T mapViewInterface);
-}
-
-class _AbstractMapViewWrapperState<T extends AbstractMapViewInterface> extends State<AbstractMapViewWrapper<T>> {
-  final T _mapViewInterface;
-  final List<FloatingMarkerTitleInfo> _floatingTitles;
-  final FMTOOptions _fmtoOptions;
-
-  _AbstractMapViewWrapperState(
-    this._mapViewInterface,
-    this._floatingTitles,
-    this._fmtoOptions,
-  );
 
   @override
   Widget build(final BuildContext context) {
     return Stack(
       children: [
-        widget.buildMapView(context, _mapViewInterface),
+        buildMapView(context, _mapViewInterface),
         FlutterMapFloatingMarkerTitlesOverlay(
           _mapViewInterface,
           _floatingTitles,
