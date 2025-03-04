5267db5f10c1b1b25e0a6170dbbc31a9bd29858a
androidseb <theandroidseb@gmail.com>
2025-03-04 16:28:00 +0100

    Upgraded example project structure

--- example/android/app/src/main/kotlin/com/example/flutter_floating_map_marker_titles_demo/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_floating_map_marker_titles_demo/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.flutter_floating_map_marker_titles_demo
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
