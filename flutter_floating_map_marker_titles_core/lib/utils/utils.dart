9163498deded321d836630eb05768e598f0f9e45
androidseb <theandroidseb@gmail.com>
2025-03-04 15:10:51 +0100

    Fixed some warnings related to color class getters

--- flutter_floating_map_marker_titles_core/lib/utils/utils.dart
+++ flutter_floating_map_marker_titles_core/lib/utils/utils.dart
@@ -4,9 +4,9 @@ class Utils {
   static const double minLuminanceToLightTinting = 0.75;
 
   static double colorLuminance(final Color color) {
-    final double red = color.red / 255.0;
-    final double green = color.green / 255.0;
-    final double blue = color.blue / 255.0;
+    final double red = color.r;
+    final double green = color.g;
+    final double blue = color.b;
 
     return 0.2126 * red + 0.7152 * green + 0.0722 * blue;
   }
